27ba74a69bebc0762feaf5922227dab9d5640dbf
Seth Battis <seth@battis.net>
2024-02-21 13:58:01 -0500

    arrays

--- src/Base.php
+++ src/Base.php
@@ -10,7 +10,7 @@ abstract class Base {
     protected function typeAs(string $type, int $flags = self::TYPE_FQN): string
     {
         if ($flags & self::TYPE_FQN) {
-            $t = preg_replace("/(.+)\\[\\]$/", "$1", $type);
+            $t = preg_replace("/(.+)\\[\]$/", "$1", $type);
             if (($flags & self::TYPE_ABSOLUTE) && !in_array($t, ['void', 'null','bool','int','float','string','array','object','callable','resource'])) {
                 $type = "\\" . $type;
             }
